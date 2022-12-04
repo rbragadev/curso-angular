a5f88c75458cc5619fb1fdcc1d87ca2c53ad93b9
Raphael Braga <106981015+rbragadev@users.noreply.github.com>
2022-12-04 07:46:15 -0300

    studyDirective

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName = 'Raphael';
+  userData = {
+    email: 'raffael.info@gmail.com',
+    role: 'admin',
+  };
+
   title = 'curso-angular';
 }
