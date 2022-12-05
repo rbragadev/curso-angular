5af8afd257286c1f40eacf9d5b782cd5906e1b13
Raphael Braga <106981015+rbragadev@users.noreply.github.com>
2022-12-05 18:29:35 -0300

    aula18-http

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -9,16 +9,13 @@ import { ListService } from 'src/app/services/list.service';
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent {
-  animals: Animal[] = [
-    { name: 'Turca', type: 'Dog', age: 4 },
-    { name: 'Tom', type: 'Cat', age: 10 },
-    { name: 'Frida', type: 'Dog', age: 5 },
-    { name: 'Bob', type: 'Horse', age: 1 },
-  ];
+  animals: Animal[] = [];
 
   animalDetails = '';
 
-  constructor(private listService: ListService) {}
+  constructor(private listService: ListService) {
+    this.getAnimals();
+  }
 
   showAge(animal: Animal) {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
@@ -27,5 +24,9 @@ export class ListRenderComponent {
   removeAnimal(animal: Animal) {
     console.log('Removendo animal');
     this.animals = this.listService.remove(this.animals, animal);
+  }
+
+  getAnimals(): void {
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
   }
 }
