f9d2861aeaa435f923ba2809a23d36015213aa84
Raphael Braga <106981015+rbragadev@users.noreply.github.com>
2022-12-05 13:00:29 -0300

    aula14-Pipes

---
+++ src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.css'],
+})
+export class PipesComponent {
+  someText: string = 'Testando o pipe operator';
+  today = new Date();
+}
